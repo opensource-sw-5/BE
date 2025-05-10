c1ea4db00ba0d0056a58c67784ded88dc901f0dc
Jhzlo <kjh0010703@naver.com>
2025-05-11 02:26:17 +0900

    feat: controller 생성

--- src/main/java/com/vata/profile/controller/ProfileController.java
+++ src/main/java/com/vata/profile/controller/ProfileController.java
@@ -1,7 +1,25 @@
 package com.vata.profile.controller;
 
-import org.springframework.stereotype.Controller;
+import com.vata.profile.application.ProfilePromptFacade;
+import com.vata.profile.controller.dto.UserInputRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/api/profile")
+@RequiredArgsConstructor
 public class ProfileController {
+
+    private final ProfilePromptFacade profilePromptFacade;
+
+    @PostMapping("/generate")
+    public ResponseEntity<String> generatePrompt(@RequestBody UserInputRequest request) {
+        String prompt = profilePromptFacade.generatePrompt(request);
+        // TODO: 이미지 생성
+        return ResponseEntity.ok(prompt);
+    }
 }
