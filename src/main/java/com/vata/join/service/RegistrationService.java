bbb23ded582ceba3c2603ff7fa92c50074d7b3ed
MoonHyungJu <moon4528@dankook.ac.kr>
2025-05-12 11:18:03 +0900

    feat: RegistrationService 구현

--- src/main/java/com/vata/join/service/RegistrationService.java
+++ src/main/java/com/vata/join/service/RegistrationService.java
@@ -1,2 +1,45 @@
-package com.vata.join.service;public class RegistrationService {
+/*
+회원가입 비즈니스 로직
+    클라이언트로부터 전달된 회원가입 요청 데이터 검증
+    1. 중복된 사용자 정보 있는지 확인
+    2. 새로운 사용자 정보 데이터베이스 저장
+ */
+package com.vata.join.service;
+
+import com.vata.join.domain.User;
+import com.vata.join.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service    // 스프링의 서비스 컴포넌트로 등록
+@RequiredArgsConstructor    // final 필드에 대한 생성자를 자동으로 생성
+public class RegistrationService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public void signup(SignupRequest signupRequest) {
+        // 중복 검사
+        if (userRepository.findByUsername(signupRequest.getUsername()).isPresent()) {
+            throw new IllegalArgumentException("이미 사용 중인 아이디입니다.");
+        }
+        if (userRepository.findByNickname(signupRequest.getUNickname()).isPresent()) {
+            throw new IllegalArgumentException("이미 사용 중인 닉네임입니다.");
+        }
+        if (userRepository.findByEmail(signupRequest.getEmail()).isPresent()) {
+            throw new IllegalArgumentException("이미 사용 중인 이메일 주소입니다.");
+        }
+
+        User user = new User();
+        user.setUsername(signupRequest.getUsername());
+        user.setPassword(passwordEncoder.encode(signupRequest.getPassword()));
+        // 비밀번호 암호화하여 User 객체에 저장
+        user.setNickname(signupRequest.getNickname());
+        user.setEmail(signupRequest.getEmail());
+
+        userRepository.save(user);  //JpaRepository 로부터 상속
+    }
+
 }
