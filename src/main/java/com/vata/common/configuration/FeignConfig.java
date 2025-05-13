f9b38229b9ba4338723df41475a9f69db0ab7535
Jhzlo <kjh0010703@naver.com>
2025-05-13 11:04:09 +0900

    fix: FeignClient 빈 등록

--- src/main/java/com/vata/common/configuration/FeignConfig.java
+++ src/main/java/com/vata/common/configuration/FeignConfig.java
@@ -1,10 +1,13 @@
 package com.vata.common.configuration;
 
+import com.vata.profile.infrastructure.StabilityImageFeignClient;
 import feign.form.spring.SpringFormEncoder;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableFeignClients(clients = StabilityImageFeignClient.class)
 public class FeignConfig {
 
     @Bean
--- src/main/java/com/vata/profile/infrastructure/StabilityImageFeignClient.java
+++ src/main/java/com/vata/profile/infrastructure/StabilityImageFeignClient.java
@@ -1,7 +1,7 @@
 package com.vata.profile.infrastructure;
 
 import com.vata.common.configuration.FeignConfig;
-import jakarta.annotation.Resource;
+import org.springframework.core.io.Resource;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
