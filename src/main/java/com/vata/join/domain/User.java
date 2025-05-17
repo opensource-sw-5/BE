749d75424a53da73bf42ab4f8550262298c16afc
MoonHyungJu <moon4528@dankook.ac.kr>
2025-05-17 23:34:40 +0900

    feat(#6): delete username field refactor(#6): Apply @Getter and @Setter

--- src/main/java/com/vata/join/domain/User.java
+++ src/main/java/com/vata/join/domain/User.java
@@ -4,58 +4,20 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String username;    // 로그인 아이디
+    private String email;    // 이메일(아이디)
     private String password;    // 비밀번호
     private String nickname;    // 닉네임
-    private String email;   // 이메일
-
-    // Getter & Setter
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getNickname() {
-        return nickname;
-    }
-
-    public void setNickname(String nickname) {
-        this.nickname = nickname;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 
 }
