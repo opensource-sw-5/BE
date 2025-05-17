6ee9b275e005b0cfc1cb93f4b55730e887388136
MoonHyungJu <moon4528@dankook.ac.kr>
2025-05-17 21:08:42 +0900

    feat(#6): Update signup API endpoint and permit Swagger UI access

--- src/main/java/com/vata/join/config/SecurityConfig.java
+++ src/main/java/com/vata/join/config/SecurityConfig.java
@@ -8,9 +8,17 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+
 @Configuration
 @EnableWebSecurity  // 스프링 시큐리티 기능 활성화
 public class SecurityConfig {
+
+    private static final String[] PERMIT_ALL_PATTERNS = {
+            "/api/auth/**",
+            "/swagger-ui/**",
+            "/v3/api-docs/**",
+            "/docs/**"
+    };
 
     @Bean
     //  BCryptPasswordEncoder 빈을 생성하여 스프링 컨테이너에 등록
@@ -23,9 +31,9 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf((csrf) -> csrf.disable())// 개발 편의상 CSRF 비활성화 (실제 서비스에서는 활성화 권장)
-                .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/service/signup").permitAll() // 회원가입 경로는 인증 없이 접근 허용
-                        .anyRequest().authenticated() // 그 외 모든 요청은 인증 필요
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(PERMIT_ALL_PATTERNS).permitAll()
+                        .anyRequest().authenticated()
                 );
         return http.build();
     }
