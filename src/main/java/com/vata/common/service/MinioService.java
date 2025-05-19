6b187a8e74e1e40b7304964759d7dc3fc705108f
Jhzlo <kjh0010703@naver.com>
2025-05-20 00:26:36 +0900

    feat: minio service 코드 작성

---
+++ src/main/java/com/vata/common/service/MinioService.java
@@ -0,0 +1,54 @@
+package com.vata.common.service;
+
+import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
+import io.minio.RemoveObjectArgs;
+import org.springframework.stereotype.Component;
+
+import java.io.InputStream;
+
+@Component
+public class MinioService {
+
+    private final MinioClient minioClient;
+
+    private static final String BUCKET_NAME = "vata";
+    private static final String PUBLIC_URL = "http://localhost:9000";
+
+    public MinioService(MinioClient minioClient) {
+        this.minioClient = minioClient;
+    }
+
+    public String uploadFile(String path, InputStream inputStream, long contentLength, String contentType) {
+        try {
+            minioClient.putObject(
+                    PutObjectArgs.builder()
+                            .bucket(BUCKET_NAME)
+                            .object(path)
+                            .stream(inputStream, contentLength, -1)
+                            .contentType(contentType)
+                            .build()
+            );
+            return getPublicUrl(path);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to upload file to MinIO", e);
+        }
+    }
+
+    public void deleteObject(String path) {
+        try {
+            minioClient.removeObject(
+                    RemoveObjectArgs.builder()
+                            .bucket(BUCKET_NAME)
+                            .object(path)
+                            .build()
+            );
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to delete object from MinIO", e);
+        }
+    }
+
+    private String getPublicUrl(String path) {
+        return String.format("%s/%s/%s", PUBLIC_URL, BUCKET_NAME, path);
+    }
+}
