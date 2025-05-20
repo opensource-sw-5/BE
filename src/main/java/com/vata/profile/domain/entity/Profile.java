357e89926a386b2b9b423714ce53d906491ab4b6
Jhzlo <kjh0010703@naver.com>
2025-05-20 10:43:46 +0900

    feat: profile 엔티티 생성

---
+++ src/main/java/com/vata/profile/domain/entity/Profile.java
@@ -0,0 +1,32 @@
+package com.vata.profile.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "profile")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Profile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Column(name = "profile_url", nullable = false)
+    private String profileUrl;
+}
