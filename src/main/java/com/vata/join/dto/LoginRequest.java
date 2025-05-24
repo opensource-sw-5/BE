cd3516bb252def29b711272d4684ab32d06259a0
MoonHyungJu <moon4528@dankook.ac.kr>
2025-05-25 00:15:55 +0900

    Refactor(#12): Apply record in LoginRequest

--- src/main/java/com/vata/join/dto/LoginRequest.java
+++ src/main/java/com/vata/join/dto/LoginRequest.java
@@ -1,15 +1,3 @@
 package com.vata.join.dto;
 
-import jakarta.validation.constraints.NotBlank;
-import lombok.Data;
-
-@Data
-public class LoginRequest {
-
-    @NotBlank(message = "아이디(이메일)을 입력해주세요.")
-    private String email;
-
-    @NotBlank(message = "비밀번호를 입력해주세요.")
-    private String password;
-
-}
+public record LoginRequest (String email, String password) {}
