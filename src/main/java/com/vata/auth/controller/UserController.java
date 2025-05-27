4a375fab68bca5e39b81601fa65f353592ed2397
Jhzlo <kjh0010703@naver.com>
2025-05-27 11:05:13 +0900

    feat: AccessKey 등록 api 생성

--- src/main/java/com/vata/auth/controller/UserController.java
+++ src/main/java/com/vata/auth/controller/UserController.java
@@ -3,11 +3,15 @@ package com.vata.auth.controller;
 import com.vata.auth.application.UserFacade;
 import com.vata.auth.controller.swagger.UserControllerSpec;
 import com.vata.auth.domain.entity.User;
+import com.vata.auth.dto.AccessKeyCreateRequest;
 import com.vata.auth.dto.UserResponse;
 import com.vata.common.annotation.LoginUser;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +25,14 @@ public class UserController implements UserControllerSpec {
     public ResponseEntity<UserResponse> getMyInfo(@LoginUser Long userId) {
         UserResponse response = userFacade.getUserInfo(userId);
         return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/access-key")
+    public ResponseEntity<String> saveAccessKey(
+            @LoginUser Long userId,
+            @Valid @RequestBody AccessKeyCreateRequest request
+    ) {
+        userFacade.saveAccessKey(userId, request);
+        return ResponseEntity.ok("AccessKey 등록이 완료되었습니다.");
     }
 }
--- src/main/java/com/vata/auth/controller/swagger/UserControllerSpec.java
+++ src/main/java/com/vata/auth/controller/swagger/UserControllerSpec.java
@@ -1,14 +1,24 @@
 package com.vata.auth.controller.swagger;
 
+import com.vata.auth.dto.AccessKeyCreateRequest;
 import com.vata.auth.dto.UserResponse;
 import com.vata.common.annotation.LoginUser;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Tag(name = "User API", description = "회원 정보 API")
 public interface UserControllerSpec {
 
     @Operation(summary = "내 정보 조회", description = "현재 로그인한 사용자의 정보를 반환합니다.")
     ResponseEntity<UserResponse> getMyInfo(@LoginUser Long userId);
+
+    @Operation(summary = "access key 등록", description = "stability ai access key를 등록합니다.")
+    ResponseEntity<String> saveAccessKey(
+            @LoginUser Long userId,
+            @Parameter(description = "access key 정보") @Valid @RequestBody AccessKeyCreateRequest request
+    );
 }
