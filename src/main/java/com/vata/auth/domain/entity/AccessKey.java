b640e78e5aba6f2d1ab7c92c0adb38ff87b50212
Jhzlo <kjh0010703@naver.com>
2025-05-27 11:02:52 +0900

    feat: AccessKey 저장 로직 구성

--- src/main/java/com/vata/auth/domain/entity/AccessKey.java
+++ src/main/java/com/vata/auth/domain/entity/AccessKey.java
@@ -1,5 +1,6 @@
 package com.vata.auth.domain.entity;
 
+import com.vata.auth.dto.AccessKeyCreateRequest;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -13,6 +14,7 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
+import org.checkerframework.checker.units.qual.A;
 
 @Entity
 @Table(name = "access-key")
@@ -29,4 +31,11 @@ public class AccessKey {
 
     @Column(name = "value", nullable = false)
     private String value;
+
+    public static AccessKey of (Long userId, String value){
+        return AccessKey.builder()
+                .userId(userId)
+                .value(value)
+                .build();
+    }
 }
---
+++ src/main/java/com/vata/auth/domain/service/AccessKeyService.java
@@ -0,0 +1,19 @@
+package com.vata.auth.domain.service;
+
+import com.vata.auth.domain.entity.AccessKey;
+import com.vata.auth.domain.repository.AccessKeyRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class AccessKeyService {
+    private final AccessKeyRepository accessKeyRepository;
+
+    @Transactional
+    public void save(Long userId, String value) {
+        AccessKey key = AccessKey.of(userId, value);
+        accessKeyRepository.save(key);
+    }
+}
