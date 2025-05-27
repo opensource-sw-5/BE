8f97f2c773822dd6fcbd4b30995bf62e8adc2e7b
Jhzlo <kjh0010703@naver.com>
2025-05-27 11:11:19 +0900

    feat: create_at를 기준으로 access key 반환

--- src/main/java/com/vata/auth/domain/entity/AccessKey.java
+++ src/main/java/com/vata/auth/domain/entity/AccessKey.java
@@ -1,27 +1,23 @@
 package com.vata.auth.domain.entity;
 
-import com.vata.auth.dto.AccessKeyCreateRequest;
+import com.vata.common.jpa.shared.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.NoArgsConstructor;
-import org.checkerframework.checker.units.qual.A;
+import lombok.experimental.SuperBuilder;
 
 @Entity
 @Table(name = "access-key")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
-@Builder
-public class AccessKey {
+@SuperBuilder
+public class AccessKey extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -32,7 +28,7 @@ public class AccessKey {
     @Column(name = "value", nullable = false)
     private String value;
 
-    public static AccessKey of (Long userId, String value){
+    public static AccessKey of(Long userId, String value) {
         return AccessKey.builder()
                 .userId(userId)
                 .value(value)
