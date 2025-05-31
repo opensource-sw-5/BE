0356460584427dd73c23c1554446341d1c714fb7
Jhzlo <kjh0010703@naver.com>
2025-06-01 00:47:07 +0900

    feat: offset 기반 pagination 메서드 정의

--- src/main/java/com/vata/profile/domain/service/ProfileService.java
+++ src/main/java/com/vata/profile/domain/service/ProfileService.java
@@ -4,6 +4,8 @@ import com.vata.profile.domain.entity.Profile;
 import com.vata.profile.domain.repository.ProfileRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,5 +20,9 @@ public class ProfileService {
                 .profileUrl(imageUrl)
                 .build();
         return profileRepository.save(profile);
+    }
+
+    public Page<Profile> findAllByUserId(Long userId, Pageable pageable) {
+        return profileRepository.findAllByUserId(userId, pageable);
     }
 }
