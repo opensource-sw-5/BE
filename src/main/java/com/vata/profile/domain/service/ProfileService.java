a2d7e625072d3496ab6a99b47df4b7e481f1f87c
Jhzlo <kjh0010703@naver.com>
2025-06-01 00:55:19 +0900

    refactor: readOnly true 설정

--- src/main/java/com/vata/profile/domain/service/ProfileService.java
+++ src/main/java/com/vata/profile/domain/service/ProfileService.java
@@ -2,11 +2,11 @@ package com.vata.profile.domain.service;
 
 import com.vata.profile.domain.entity.Profile;
 import com.vata.profile.domain.repository.ProfileRepository;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -22,6 +22,7 @@ public class ProfileService {
         return profileRepository.save(profile);
     }
 
+    @Transactional(readOnly = true)
     public Page<Profile> findAllByUserId(Long userId, Pageable pageable) {
         return profileRepository.findAllByUserId(userId, pageable);
     }
