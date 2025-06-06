47cfc8fd88e1f96874d4848c1a0ab55711f554c5
Jhzlo <kjh0010703@naver.com>
2025-06-06 13:32:21 +0900

    refactor: 구조 통일

--- src/main/java/com/vata/profile/application/ProfileFacade.java
+++ src/main/java/com/vata/profile/application/ProfileFacade.java
@@ -1,7 +1,9 @@
 package com.vata.profile.application;
 
+import com.vata.auth.domain.service.AccessKeyService;
 import com.vata.profile.controller.dto.ProfileListResponse;
 import com.vata.profile.domain.service.ProfileService;
+import com.vata.profile.infrastructure.StabilityRestClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -13,6 +15,8 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class ProfileFacade {
     private final ProfileService profileService;
+    private final AccessKeyService accessKeyService;
+    private final StabilityRestClient stabilityRestClient;
 
     public Page<ProfileListResponse> getProfileList(Long userId, int page, int size) {
         Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "id"));
@@ -23,5 +27,13 @@ public class ProfileFacade {
                         profile.getProfileUrl(),
                         profile.getCreatedAt()
                 ));
+    }
+
+    public double getUserStabilityCredits(Long userId) {
+        // 1. userId를 통해 Access Key를 조회
+        String apiKey = accessKeyService.getValue(userId);
+
+        // 2. StabilityRestClient를 통해 Stability AI API 호출하여 크레딧 잔액 조회
+        return stabilityRestClient.getBalanceCredits(apiKey);
     }
 }
